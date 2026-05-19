c9fcdf87fb700b57bc4a8f37274f737672548e80
Lautaro Colella <lautarocolella1@gmail.com>
2026-05-19 17:37:59 -0300

    add controller wrapper to remove repetitive infrastructural noise

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,20 +1,17 @@
 const dashboardService = require("../services/dashboardService");
+const asyncHandler = require("../middlewares/asyncHandler");
 
-const getDashboardMetrics = async (req, res, next) => {
-  try {
-    const { from, to, period } = req.query;
+const getDashboardMetrics = asyncHandler(async (req, res) => {
+  const { from, to, period } = req.query;
 
-    const metrics = await dashboardService.getDashboardMetrics({
-      from,
-      to,
-      period,
-    });
+  const metrics = await dashboardService.getDashboardMetrics({
+    from,
+    to,
+    period,
+  });
 
-    return res.status(200).json(metrics);
-  } catch (err) {
-    next(err);
-  }
-};
+  return res.status(200).json(metrics);
+});
 
 module.exports = {
   getDashboardMetrics,
--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -1,26 +1,19 @@
 const ticketService = require("../services/ticketService");
+const asyncHandler = require("../middlewares/asyncHandler");
 
-const getAllTickets = async (req, res, next) => {
-  try {
-    const { page = 1, limit = 10, title } = req.query;
+const getAllTickets = asyncHandler(async (req, res) => {
+  const { page = 1, limit = 10, title } = req.query;
 
-    const tickets = await ticketService.getAllTickets({ page, limit, title });
+  const tickets = await ticketService.getAllTickets({ page, limit, title });
 
-    return res.status(200).json(tickets);
-  } catch (err) {
-    next(err);
-  }
-};
+  return res.status(200).json(tickets);
+});
 
-const createTicket = async (req, res, next) => {
-  try {
-    const ticket = await ticketService.createTicket(req.body);
+const createTicket = asyncHandler(async (req, res) => {
+  const ticket = await ticketService.createTicket(req.body);
 
-    return res.status(201).json(ticket);
-  } catch (err) {
-    next(err);
-  }
-};
+  return res.status(201).json(ticket);
+});
 
 module.exports = {
   getAllTickets,
