518c9646fd2cc88936d42a008e042678eb039d24
Lautaro Colella <lautarocolella1@gmail.com>
2026-06-04 08:12:29 -0300

    password hash with bcrypt

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,13 @@
+const bcrypt = require("bcrypt");
+
 const userRepository = require("../repositories/userRepository");
 
 const createUser = async (userData) => {
-  return await userRepository.create(userData);
+  const hashedPassword = await bcrypt.hash(userData.password, 10);
+  return await userRepository.create({
+    ...userData,
+    password: hashedPassword,
+  });
 };
 
 module.exports = {
