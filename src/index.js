2a0bb343aee4564ae8cfcd8146c07d2bd5fc98e0
Lautaro Colella <lautarocolella1@gmail.com>
2026-06-16 19:09:41 -0300

    update SERVER_PORT env variable for render

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(notFound);
 // * The error handling middleware MUST be the LAST middleware
 app.use(errorHandler);
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 AppDataSource.initialize()
   .then(() => {
